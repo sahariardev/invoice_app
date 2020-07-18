7b8d351a3dfc292e2f4c20dc7a5abf8905649cd1
sahariardev <sahariaralamk27@gmail.com>
2020-07-18 22:03:36 +0600

    Added bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:invoice_generator/pages/BottomNavBar.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(child: Text("Hello World"),),
+      home: BottomNavBar(),
     );
   }
 }
