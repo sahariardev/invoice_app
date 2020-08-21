b34ed195a3259f6f15f1ae481e3f0f118843b4f4
sabbir-anwar <protik26@gmail.com>
2020-08-21 10:40:19 +0600

    added getCustomButton from widget_utils

--- lib/pages/Info.dart
+++ lib/pages/Info.dart
@@ -124,21 +124,23 @@ class Info extends StatelessWidget {
 //          onPressed: () => _showCustomDatePicker(context, initialDate, action),
 //        ),
         const SizedBox(height: 10),
-        RaisedButton(
-          onPressed: () => _showCustomDatePicker(context, initialDate, action),
-          color: Colors.amber[900],
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(18.0),
-              side: BorderSide(color: Colors.black)
-          ),
-          child: Container(
-            height: 50,
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            child: Center(
-              child: Text(pickerName, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-            ),
-          ),
-        ),
+        WidgetUtil.getCustomButton(pickerName, () => _showCustomDatePicker(context, initialDate, action)),
+//        RaisedButton(
+//          onPressed: () => _showCustomDatePicker(context, initialDate, action),
+//          color: Colors.amber[900],
+//          shape: RoundedRectangleBorder(
+//              borderRadius: BorderRadius.circular(18.0),
+//              side: BorderSide(color: Colors.black)
+//          ),
+//          child: Container(
+//            height: 50,
+//            width: 120,
+//            margin: EdgeInsets.symmetric(horizontal: 20),
+//            child: Center(
+//              child: Text(pickerName, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+//            ),
+//          ),
+//        ),
       ],
     );
   }
